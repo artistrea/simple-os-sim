9745f0501f4ba34b84ec7eeddd615529d4811a41
Bernardo Lobo <bernardolobo3@gmail.com>
2025-12-03 10:23:35 -0300

    file system implementation

---
+++ src/simple_os/files/disk.py
@@ -0,0 +1,80 @@
+"""
+Module that defines basic structures for disk and block representation.
+Each disk block is represented by a DiskBlock object.
+The entire disk is represented as a collection of blocks.
+"""
+
+class DiskBlock:
+    """Represents an individual disk block"""
+    
+    def __init__(self, index, file_id='0'):
+        """Initializes a disk block"""
+        self.index = index  # Block position in the disk
+        self.file_id = file_id  # '0' = free block, letter = file occupying
+    
+    def __str__(self):
+        """Returns string representation of the block"""
+        if self.file_id == '0':
+            return f"Block {self.index}: Free"
+        else:
+            return f"Block {self.index}: File '{self.file_id}'"
+    
+    def free(self):
+        """Frees the block, making it available"""
+        self.file_id = '0'
+    
+    def occupy(self, file_id):
+        """Occupies the block with a specific file"""
+        if self.file_id != '0':
+            return False  # Block already occupied
+        self.file_id = file_id
+        return True
+
+
+class Disk:
+    """Represents the entire disk as a collection of blocks"""
+    
+    def __init__(self, total_blocks):
+        """Initializes the disk with all blocks free"""
+        self.total_blocks = total_blocks
+        
+        # Initialize all blocks as free
+        self.blocks = [DiskBlock(i) for i in range(total_blocks)]
+        
+        # Dictionary to map files (ID -> information)
+        self.files = {}
+    
+    def get_block(self, index):
+        """Returns the block at the specified index"""
+        if 0 <= index < self.total_blocks:
+            return self.blocks[index]
+        return None
+    
+    def is_free(self, index):
+        """Checks if a block is free"""
+        block = self.get_block(index)
+        return block is not None and block.file_id == '0'
+    
+    def occupy_block(self, index, file_id):
+        """Occupies a specific block with a file"""
+        block = self.get_block(index)
+        if block:
+            return block.occupy(file_id)
+        return False
+    
+    def free_block(self, index):
+        """Frees a specific block"""
+        block = self.get_block(index)
+        if block:
+            block.free()
+            return True
+        return False
+    
+    def get_state(self):
+        """Returns current disk state as a list"""
+        return [block.file_id for block in self.blocks]
+    
+    def __str__(self):
+        """Returns string representation of the disk"""
+        state = self.get_state()
+        return f"Disk with {self.total_blocks} blocks. State: {state}"
---
+++ src/simple_os/files/file.py
@@ -0,0 +1,67 @@
+"""
+Module that defines the File class and related operations.
+Manages file metadata and permissions.
+"""
+
+class File:
+    """Represents a file in the file system"""
+    
+    def __init__(self, file_id, name, size, owner_process, start_block):
+        """Initializes a new file"""
+        self.id = file_id  # File identifier (a single letter)
+        self.name = name  # File name
+        self.size = size  # Size in blocks
+        self.owner_process = owner_process  # Process that created the file
+        self.start_block = start_block  # First occupied block
+        
+        # List of all blocks occupied by this file
+        self.blocks = list(range(start_block, start_block + size))
+    
+    def __str__(self):
+        """Returns string representation of the file"""
+        return (f"File '{self.id}' ('{self.name}'): "
+                f"Owner: P{self.owner_process}, "
+                f"Size: {self.size} blocks, "
+                f"Start block: {self.start_block}, "
+                f"Blocks: {self.blocks}")
+    
+    def get_blocks(self):
+        """Returns the list of blocks occupied by the file"""
+        return self.blocks
+    
+    def belongs_to_process(self, process_id):
+        """hecks if the file belongs to a specific process"""
+        return self.owner_process == process_id
+
+
+class FileOperation:
+    """Represents an operation to be executed in the file system"""
+    
+    # Constants for operation types
+    CREATE = 1
+    DELETE = 2
+    
+    def __init__(self, process_id, operation_type, file_name, size=0, file_id=None):
+        """Initializes a file operationInitializes a file operation"""
+        self.process_id = process_id
+        self.type = operation_type
+        self.file_name = file_name
+        self.size = size
+        self.file_id = file_id
+    
+    def __str__(self):
+        """Returns string representation of the operation"""
+        if self.type == self.CREATE:
+            return (f"CREATE operation: Process P{self.process_id}, "
+                    f"File '{self.file_name}', Size: {self.size} blocks")
+        else:
+            return (f"DELETE operation: Process P{self.process_id}, "
+                    f"File '{self.file_name}' (ID: {self.file_id})")
+    
+    def is_creation(self):
+        """Checks if it's a create operation"""
+        return self.type == self.CREATE
+    
+    def is_deletion(self):
+        """Checks if it's a delete operation"""
+        return self.type == self.DELETE

25c3f8dec0b7488fc808d1708a1ae70b9f6b24a5
phbm2003 <145854692+phbm2003@users.noreply.github.com>
2025-12-03 11:35:28 -0300

    Create Memory.py

---
+++ src/simple_os/memory/Memory.py
@@ -0,0 +1,9 @@
+# This Python file uses the following encoding: utf-8
+
+class Memory:
+    def __init__(self):
+        self.total_blocks = 1024
+        self.blocks = [None] * self.total_blocks
+
+    def __repr__(self):
+        return str(self.blocks) # retorna lista self.blocks convertida pra string
